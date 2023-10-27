f1eccad24735b21d473fa219613d0ef132f86865
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-10-26 21:11:54 -0300

    Projeto criado

---
+++ lojaroupas-api/src/main/java/org/lojaroupas/api/LojaroupasApiApplication.java
@@ -0,0 +1,13 @@
+package org.lojaroupas.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaroupasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaroupasApiApplication.class, args);
+	}
+
+}
---
+++ lojaroupas-api/src/test/java/org/lojaroupas/api/LojaroupasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lojaroupas.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaroupasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
